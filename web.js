e92e363700ad86fb37039ba3a7b0984ae9cf485c
Julien-559 <ja2740@columbia.edu>
2013-08-06 00:15:51 +0000

    done

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFileSync(index.html);
+  response.send('moi jaime');
 });
 
 var port = process.env.PORT || 5000;
